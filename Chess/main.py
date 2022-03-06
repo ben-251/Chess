0d84301e54d2ea718e02a7b4bfbacee0d52c60e7
Ben <92220618+cfop-user@users.noreply.github.com>
2022-03-06 02:29:44 +0000

    Update main.py

--- Chess/main.py
+++ Chess/main.py
@@ -1,3 +1,10 @@
+#define full board
+board = []
+for y in range(8):
+    for x in range(8):
+        board.append([y+1,x+1])
+
+
 class player:
     def __init__(self,turn,side):
         self.turn = turn
@@ -23,24 +30,19 @@ class bishop(piece):
         if delta_x != delta_y:
             start()
         return end_position
-        
-def invalid_move(function):
-    print("Invalid Move.")
-    function()
 
 player1 = player(True,"white")
 player2 = player(False, "black")
 
 white_bishop_1 = bishop("white",True,[3,1])
 white_bishop_2 = bishop("white",True,[6,1])
-white_bishops = [white_bishop_1,white_bishop_2]
+white_pieces = [white_bishop_1,white_bishop_2]
 
 black_bishop_1 = bishop("black",True,[3,8])
 black_bishop_2 = bishop("black",True,[6,8])
-black_bishops = [white_bishop_1,white_bishop_2]
+black_pieces = [white_bishop_1,white_bishop_2]
 
-def get_array(piece_positions): #I can refine this later but for now need to focus
-    
+def get_array(piece_positions,stage): #I can refine this later but for now need to focus   
     x_values = []
     y_values = []
 
@@ -49,13 +51,13 @@ def get_array(piece_positions): #I can refine this later but for now need to foc
     for i in piece_positions:
         y_values.append(i[1])
 
-    x_position = int(input("enter first value for start position: "))
+    x_position = int(input(f"enter first value for {stage} position: "))
     while x_position not in x_values:
-        x_position = int(input("Try again.\nEnter first value for start position: "))
+        x_position = int(input(f"Try again.\nEnter first value for {stage} position: "))
 
-    y_position = int(input("enter second value for start position: "))
+    y_position = int(input(f"enter second value for {stage} position: "))
     while y_position not in y_values:
-        y_position = int(input("Try again.\nEnter first value for start position: "))
+        y_position = int(input(f"Try again.\nEnter first value for {stage} position: "))
 
     return [x_position,y_position]
 
@@ -70,28 +72,41 @@ def swap():
         player1.turn = True
     else: print
 
-def determine_valid_bishop_squares():
-    all_squares = [[0,0]]*64
+def determine_valid_bishop_squares(init_position,all_pieces): #ned to find a way to get all the pieces' positions
     x = 0
     y = 0
+    squares = board
 
-    #generate array with all sqaures
-    for i in range(8):
-        for i in range(8):
-            valid_squares[i] = [x,y]
-            x+=1
+    for i in squares:
+        delta_x = abs(i[0] - init_position[0])
+        delta_y = abs(i[1] - init_position[1])
+
+        if delta_x != delta_y:
+            squares.remove(i)
         
+        else:
+            for i in all_pieces:
+                all_piece_positions = i.position
 
-def start():   
-    start_position = get_array([white_bishops[0].position,white_bishops[1].position])
+            if i in all_piece_positions:
+                squares.remove(i)
+    return squares
+
+def invalid_move(function):
+    print("Invalid Move.")
+    function()
+
+def start(): 
+    start_position = get_array([white_pieces[0].position,white_pieces[1].position],"start")
 
     if start_position == white_bishop_1.position:
         chosen_piece = white_bishop_1
     else:
         chosen_piece = white_bishop_2
     
-    valid_squares = determine_valid_squares()
-    end_position = get_array(valid_squares)
+    valid_squares = determine_valid_bishop_squares(chosen_piece.position,white_pieces)
+    print(f"the squares your bishop can move to are{valid_squares}.")
+    end_position = get_array(valid_squares,"end")
     if chosen_piece.__class__== bishop:
         chosen_piece.position = bishop.move(start_position,end_position)
         swap()

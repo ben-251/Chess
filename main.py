01a6a6b1830d404fb6a173b2022e70a4d9ad13e5
ben <yipjones2@gmail.com>
2022-03-10 21:35:59 +0000

    support for knights, rooks, and queens added

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class bishop(piece):
     def __init__(self,side,alive,position):
        super().__init__(side,alive,position)
 
-    def determine_valid_squares(init_position,all_pieces,board):
+    def determine_valid_squares(self,init_position,all_pieces,board):
         x = 0
         y = 0
         squares = board
@@ -47,7 +47,7 @@ class knight(piece):
     def __init__(self,side,alive,position):
        super().__init__(side,alive,position)
 
-    def determine_valid_squares(init_position,all_pieces,board):
+    def determine_valid_squares(self,init_position,all_pieces,board):
         x = 0
         y = 0
         squares = board
@@ -61,13 +61,9 @@ class knight(piece):
             delta_x = abs(i[0] - init_position[0])
             delta_y = abs(i[1] - init_position[1])
 
-            if delta_x != delta_y:
-                #squares.remove(i) #removing squares in the middle of the loop messes up the index, instead store in an array of sqaures to be removed.
-                removed_squares.append(i)
+            if (not(delta_x == 1 and delta_y ==2)) and (not(delta_x == 2 and delta_y == 1)):
+                removed_squares.append(i) 
             else:
-                # for piece in all_pieces:
-                #     piece.position = i
-
                 if i in all_positions:
                     removed_squares.append(i)
             
@@ -76,6 +72,63 @@ class knight(piece):
             squares.remove(i)
         return squares        
 
+class rook(piece):
+    def __init__(self,side,alive,position):
+       super().__init__(side,alive,position)
+
+    def determine_valid_squares(self,init_position,all_pieces,board):
+        x = 0
+        y = 0
+        squares = board
+        removed_squares = []
+
+        all_positions = []
+        for i in all_pieces:
+            all_positions.append(i.position)
+
+        for i in squares:
+            delta_x = abs(i[0] - init_position[0])
+            delta_y = abs(i[1] - init_position[1])
+
+            if (not(delta_x > 0 and delta_y == 0)) and (not(delta_x == 0 and delta_y > 0)):
+                removed_squares.append(i) 
+            else:
+                if i in all_positions:
+                    removed_squares.append(i)
+            
+                    
+        for i in removed_squares:
+            squares.remove(i)
+        return squares        
+
+class queen(piece):
+    def __init__(self,side,alive,position):
+       super().__init__(side,alive,position)
+
+    def determine_valid_squares(self,init_position,all_pieces,board):
+        x = 0
+        y = 0
+        squares = board
+        removed_squares = []
+
+        all_positions = []
+        for i in all_pieces:
+            all_positions.append(i.position)
+
+        for i in squares:
+            delta_x = abs(i[0] - init_position[0])
+            delta_y = abs(i[1] - init_position[1])
+
+            if (not(delta_x > 0 and delta_y == 0)) and (not(delta_x == 0 and delta_y > 0)) and (delta_x != delta_y):
+                removed_squares.append(i) 
+            else:
+                if i in all_positions:
+                    removed_squares.append(i)
+            
+                    
+        for i in removed_squares:
+            squares.remove(i)
+        return squares     
 
 def get_array(stage,valid_squares): #I can refine this later but for now need to focus   
     x_values = []
@@ -131,11 +184,20 @@ def moves():
 
     white_bishop_1 = bishop("white",True,[3,1])
     white_bishop_2 = bishop("white",True,[6,1])
-    white_pieces = [white_bishop_1,white_bishop_2]
+    white_knight_1 = knight("white",True,[2,1])
+    white_knight_2 = knight("white",True,[7,1])
+    white_rook_1 = rook("white",True,[1,1])
+    white_rook_2 = rook("white",True,[8,1])
+    white_queen = queen("white",True,[4,1])
+    white_pieces = [white_bishop_1,white_bishop_2,white_knight_1,white_knight_2,white_rook_1,white_rook_2,white_queen]
 
     black_bishop_1 = bishop("black",True,[3,8])
     black_bishop_2 = bishop("black",True,[6,8])
-    black_pieces = [white_bishop_1,white_bishop_2]
+    black_knight_1 = knight("black",True,[2,8])
+    black_knight_2 = knight("black",True,[7,8])
+    black_rook_1 = rook("black",True,[1,8])
+    black_rook_2 = rook("black",True,[8,8])
+    black_pieces = [black_bishop_1,black_bishop_2,black_knight_1,black_knight_2]
 
     player1 = player(True,"white",white_pieces)
     player2 = player(False, "black",black_pieces)
@@ -164,14 +226,14 @@ def moves():
             piece_valid = False
             print("Try Again.")
 
-    valid_squares = bishop.determine_valid_squares(chosen_piece.position,white_pieces,board)
-    print(f"the squares your bishop can move to are{valid_squares}.")
+    valid_squares = chosen_piece.determine_valid_squares(chosen_piece.position,white_pieces,board)
+    print(f"the squares your piece can move to are{valid_squares}.")
 
-    end_position = get_array("end",valid_squares)
+    #end_position = get_array("end",valid_squares)
 
-    chosen_piece.position = chosen_piece.move(start_position,end_position) #need to make this function, currently doesnt exist
-    print("The bishop on",start_position,"is now on",chosen_piece.position)
-    swap()
+    #chosen_piece.position = chosen_piece.move(start_position,end_position) #need to make this function, currently doesnt exist
+   #print("The piece on",start_position,"is now on",chosen_piece.position)
+    #swap()
     #end of while. returns winner when game ends
 
 def start():

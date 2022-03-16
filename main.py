a233daa7703a7b22d2c689c38d8647e698d510d2
ben <yipjones2@gmail.com>
2022-03-16 19:44:19 +0000

    Added general gameplay

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 class player:
-    def __init__(self,turn,side,pieces):
+    def __init__(self,turn,side,pieces,name):
         self.turn = turn
         self.side = side
         self.pieces = pieces
-
+        self.name = name
 class piece:
     def __init__(self,side,alive,position):
         self.side = side
@@ -20,10 +20,6 @@ class bishop(piece):
         squares = board
         removed_squares = []
 
-        all_positions = []
-        for i in all_pieces:
-            all_positions.append(i.position)
-
         for i in squares:
             delta_x = abs(i[0] - init_position[0])
             delta_y = abs(i[1] - init_position[1])
@@ -35,8 +31,8 @@ class bishop(piece):
                 # for piece in all_pieces:
                 #     piece.position = i
 
-                if i in all_positions:
-                    removed_squares.append(i)
+                if i in all_pieces:
+                    removed_squares.append(i.position)
             
                     
         for i in removed_squares:
@@ -61,10 +57,10 @@ class knight(piece):
             delta_x = abs(i[0] - init_position[0])
             delta_y = abs(i[1] - init_position[1])
 
-            if (not(delta_x == 1 and delta_y ==2)) and (not(delta_x == 2 and delta_y == 1)):
+            if (not(delta_x == 1 and delta_y ==2)) and (not(delta_x == 2 and delta_y == 1)) and not i in removed_squares:
                 removed_squares.append(i) 
             else:
-                if i in all_positions:
+                if i in all_positions and not i in removed_squares:
                     removed_squares.append(i)
             
                     
@@ -90,7 +86,7 @@ class rook(piece):
             delta_x = abs(i[0] - init_position[0])
             delta_y = abs(i[1] - init_position[1])
 
-            if (not(delta_x > 0 and delta_y == 0)) and (not(delta_x == 0 and delta_y > 0)):
+            if (not(delta_x > 0 and delta_y == 0)) and (not(delta_x == 0 and delta_y > 0)) and (not i in removed_squares):
                 removed_squares.append(i) 
             else:
                 if i in all_positions:
@@ -120,10 +116,11 @@ class queen(piece):
             delta_y = abs(i[1] - init_position[1])
 
             if (not(delta_x > 0 and delta_y == 0)) and (not(delta_x == 0 and delta_y > 0)) and (delta_x != delta_y):
-                removed_squares.append(i) 
+                if not i in removed_squares:
+                    removed_squares.append(i) 
             else:
-                if i in all_positions:
-                    removed_squares.append(i)
+                if i in all_positions and not i in removed_squares:
+                        removed_squares.append(i)
             
                     
         for i in removed_squares:
@@ -157,21 +154,18 @@ class pawn(piece):
             delta_y = i[1] - init_position[1]
 
             if i in enemy_positions:
-                if not(delta_y == 1 and delta_x == 1):
+                if not(delta_y == 1 and delta_x == 1) and not i in removed_squares:
                     removed_squares.append(i)
             else:
                 if self.first_move:
-                    if not(delta_y == 1 and delta_x == 0) and not(delta_y == 2 and delta_x == 0):
-                        if not i in removed_squares:
-                            removed_squares.append(i)
-                else:
-                    if not(delta_y == 1 and delta_x == 0):
-                        if not i in removed_squares:
-                            removed_squares.append(i)
-            
-                if i in all_positions:
-                    if not i in removed_squares:
+                    if not(delta_y == 1 and delta_x == 0) and not(delta_y == 2 and delta_x == 0) and not i in removed_squares:
                         removed_squares.append(i)
+                else:
+                    if not(delta_y == 1 and delta_x == 0) and not i in removed_squares:
+                        removed_squares.append(i)
+            
+                if i in all_positions and not i in removed_squares:
+                    removed_squares.append(i)
                                
         for i in removed_squares:
             squares.remove(i)
@@ -188,35 +182,19 @@ def get_array(stage,valid_squares): #I can refine this later but for now need to
     for i in valid_squares:
         y_values.append(i[1])
 
-    x_position = int(input(f"enter first value for {stage} position: "))
+    x_position = int(input(f"Enter first value for {stage} position: "))
     while x_position not in x_values:
         x_position = int(input(f"Try again.\nEnter first value for {stage} position: "))
 
-    y_position = int(input(f"enter second value for {stage} position: "))
+    y_position = int(input(f"Enter second value for {stage} position: "))
     while y_position not in y_values:
-        y_position = int(input(f"Try again.\nEnter first value for {stage} position: "))
+        y_position = int(input(f"Try again.\nEnter second value for {stage} position: "))
 
     return [x_position,y_position]
 
-def swap():
-    global player1
-    global player2
-    global active_player
-    if player1.turn == True and player2.turn == False:
-        player2.turn = True
-        player1.turn = False
-        active_player = player2
-    elif player1.turn == False and player2.turn == True:
-        player2.turn = False
-        player1.turn = True
-        active_player = player1
-    else: 
-        raise Exception("Neither player is playing rn????")
+# def swap(active_player,enemy_player,player1,player2):
 
-
-def invalid_move(function):
-    print("Invalid Move.")
-    function()
+#     return active_player,enemy_player
 
 def find_piece(active_player,position):
     for i in active_player.pieces:
@@ -224,7 +202,15 @@ def find_piece(active_player,position):
             return i    
     return "PieceNotFoundError"
 
-def moves(): 
+def move(piece, position, enemy_player):
+    for enemy_piece in enemy_player.pieces:
+        if position == enemy_piece.position:
+            enemy_piece.alive = False
+            enemy_player.pieces.remove(enemy_piece)
+            break
+    piece.position = position
+
+def play():   
     #define full board
     board = []
     for y in range(8):
@@ -252,18 +238,17 @@ def moves():
     black_pawn_3 = pawn("black",True,[3,7],True)
     black_pawn_4 = pawn("black",True,[4,3],True)
     black_pawn_5 = pawn("black",True,[5,7],True)
-    black_pieces = [black_bishop_1,black_bishop_2,black_knight_1,black_knight_2,black_pawn_1,black_pawn_2,black_pawn_3,black_pawn_4,black_pawn_5]
+    black_pieces = [black_rook_1,black_rook_2,black_bishop_1,black_bishop_2,black_knight_1,black_knight_2,black_pawn_1,black_pawn_2,black_pawn_3,black_pawn_4,black_pawn_5]
 
-    player1 = player(True,"white",white_pieces)
-    player2 = player(False, "black",black_pieces)
+    player1 = player(True,"white",white_pieces,"player1")
+    player2 = player(False, "black",black_pieces,"player2")
+
+    player1.name = input("Enter player1's name: ")
+    player2.name = input("Enter player2's name: ")
 
     active_player = player1
     enemy_player = player2
     #while game not over
-    all_positions = []
-
-    for i in active_player.pieces:
-        all_positions.append(i.position)
 
     all_pieces = []
     for i in white_pieces:
@@ -271,29 +256,52 @@ def moves():
     for i in black_pieces:
         all_pieces.append(i)
 
-    start_position = get_array("start",all_positions) #get_array("start",all_pieces for colour)
+    while True:
+        print(f"{active_player.name} is going now.")
 
-    piece_valid = False
-    while piece_valid == False: #see extras.txt "attribution"
-        chosen_piece = find_piece(active_player,start_position)
-        piece_valid = True
+        active_positions = []
+        for i in active_player.pieces:
+            active_positions.append(i.position)
 
-        if chosen_piece == "PieceNotFoundError":
-            piece_valid = False
-            print("Try Again.")
+        piece_valid = False
+        while piece_valid == False: #see extras.txt "attribution"
+            start_position = get_array("start",active_positions) #get_array("start",all_pieces for colour)
+            chosen_piece = find_piece(active_player,start_position)
+            piece_valid = True
 
-    valid_squares = chosen_piece.determine_valid_squares(chosen_piece.position,active_player.pieces,enemy_player.pieces,board)
-    print(f"the squares your piece can move to are{valid_squares}.")
+            if chosen_piece == "PieceNotFoundError":
+                piece_valid = False
+                print("Try Again. NO piece here.")
 
-    #end_position = get_array("end",valid_squares)
+        valid_squares = chosen_piece.determine_valid_squares(chosen_piece.position,active_player.pieces,enemy_player.pieces,board)
+        print(f"the squares your {chosen_piece.__class__.__name__} can move to are{valid_squares}.")
 
-    #chosen_piece.position = chosen_piece.move(start_position,end_position) #need to make this function, currently doesnt exist
-   #print("The piece on",start_position,"is now on",chosen_piece.position)
-    #swap()
-    #end of while. returns winner when game ends
+        square_valid = False
+        while square_valid == False:
+            end_position = get_array("end",valid_squares)
+            square_valid = True
+            if end_position not in valid_squares:
+                square_valid = False
+                print("sorry, no squares found here.")
+        
+        move(chosen_piece, end_position, enemy_player)
+        print(f"The {chosen_piece.__class__.__name__} which was on {start_position} is now on {chosen_piece.position}. It should be on {end_position}.")
+
+       # active_player,enemy_player = swap(active_player,enemy_player,player1,player2)
+        if active_player == player1 and enemy_player == player2:
+            active_player = player2
+            enemy_player = player1
+        elif active_player == player2 and enemy_player == player1:
+            active_player = player1
+            enemy_player = player2
+        else: 
+            raise Exception("Neither player is playing rn????")
+
+
+    return "placeholder won","by winning."
 
 def start():
-    winner = moves()
-    print(f"Game over. {winner} won.")
+    winner,reason = play()
+    print(f"Game over. {winner}{reason}")
 
 start()

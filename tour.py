8f9f734ae9e4a619ac5cf995970464d0e46f8fb0
ben <yipjones2@gmail.com>
2022-04-10 04:08:26 +0100

    rough idea for knights tour

--- tour.py
+++ tour.py
@@ -1,3 +1,13 @@
+import external as ext
+
+
 def start():
-	pass
+	knight = ext.knight("white", True, [4,4],True,"N")
+	visited_squares = knight.position
+
+	board = []
+	for y in range(8):
+        for x in range(8):
+            board.append([y+1, x+1])
+
 start()
